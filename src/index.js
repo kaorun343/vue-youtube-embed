8c73baf364b9a91883f94996051fb7d64ee6e213
EyePulp <eyepulp@eyepulp.net>
2017-09-26 18:10:54 -0500

    Make it work under SSR

--- src/index.js
+++ src/index.js
@@ -2,33 +2,35 @@ import { getIdFromURL, getTimeFromURL } from './utils'
 import container from './container'
 import YouTubePlayer from './player'
 
-export { YouTubePlayer, getIdFromURL, getTimeFromURL }
+export default { YouTubePlayer, getIdFromURL, getTimeFromURL,
+  install: function (Vue, options = { global: true }) {
+    container.Vue = Vue
+    YouTubePlayer.ready = YouTubePlayer.mounted
+    if (options.global) {
+      Vue.component('youtube', YouTubePlayer)
+    }
+    Vue.prototype.$youtube = { getIdFromURL, getTimeFromURL }
 
-export default function install (Vue, options = { global: true }) {
-  container.Vue = Vue
-  YouTubePlayer.ready = YouTubePlayer.mounted
-  if (options.global) {
-    Vue.component('youtube', YouTubePlayer)
-  }
-  Vue.prototype.$youtube = { getIdFromURL, getTimeFromURL }
+    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+      const tag = document.createElement('script')
+      tag.src = 'https://www.youtube.com/player_api'
+      const firstScriptTag = document.getElementsByTagName('script')[0]
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-  const tag = document.createElement('script')
-  tag.src = 'https://www.youtube.com/player_api'
-  const firstScriptTag = document.getElementsByTagName('script')[0]
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+      window.onYouTubeIframeAPIReady = function () {
+        container.YT = YT
+        const { PlayerState } = YT
 
-  window.onYouTubeIframeAPIReady = function () {
-    container.YT = YT
-    const { PlayerState } = YT
+        container.events[PlayerState.ENDED] = 'ended'
+        container.events[PlayerState.PLAYING] = 'playing'
+        container.events[PlayerState.PAUSED] = 'paused'
+        container.events[PlayerState.BUFFERING] = 'buffering'
+        container.events[PlayerState.CUED] = 'cued'
 
-    container.events[PlayerState.ENDED] = 'ended'
-    container.events[PlayerState.PLAYING] = 'playing'
-    container.events[PlayerState.PAUSED] = 'paused'
-    container.events[PlayerState.BUFFERING] = 'buffering'
-    container.events[PlayerState.CUED] = 'cued'
-
-    Vue.nextTick(() => {
-      container.run()
-    })
+        container.Vue.nextTick(() => {
+          container.run()
+        })
+      }
+    }
   }
 }
