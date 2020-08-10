2d1e479ab8b63880b9c6d4cb7690113c58d7164c
franciscorode <fran_rode@hotmail.com>
2020-08-10 14:55:30 +0200

    Fixed a litle problem related with jest testing
    Controlling if exist 'script' html tag to add youtube api 'script', if not exist a 'script' html tag append the tag to body of document. This problem appear testing a component that use it prevously using transformIgnorePatterns

--- src/index.js
+++ src/index.js
@@ -21,7 +21,12 @@ export default {
       const tag = document.createElement('script')
       tag.src = 'https://www.youtube.com/player_api'
       const firstScriptTag = document.getElementsByTagName('script')[0]
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+      
+      if (firstScriptTag) {
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      } else {
+        document.body.appendChild(tag)
+      }
 
       window.onYouTubeIframeAPIReady = function () {
         container.YT = YT
