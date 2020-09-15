430efbf95a96f40d3e4616ed69f43c2525f84e5a
Ming Shin <7344333+mingshin430@users.noreply.github.com>
2020-09-15 17:06:06 +0800

    Add embedConfig props.

--- play/index.js
+++ play/index.js
@@ -6,11 +6,13 @@ import PlayerVars from './PlayerVars.vue'
 import UpdateVideoId from './UpdateVideoId.vue'
 import List from './List.vue'
 import Volume from './Volume.vue'
+import EmbedConfig from './EmbedConfig.vue'
 
 play('Vue YouTube Embed', module)
   .add('Events', Events)
   .add('Height and Width', Size)
   .add('Player Vars', PlayerVars)
+  .add('EmbedConfig', EmbedConfig)
   .add('Update Video ID', UpdateVideoId)
   .add('List', List)
   .add('Volume', Volume)
